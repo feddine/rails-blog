ad77bffc0631d3d460b26367fe8da0f9fae5ea4b
Firas <firas.kamaleddine@gmail.com>
2013-05-06 14:09:25 -0400

    Finished rails blog
    blog created using http://guides.rubyonrails.org/getting_started.html

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirasBlog::Application.config.secret_token = 'a51ab3b5125baff053d08898277fd6627fe0567ebecb41d10b8f135d64a2e083116330c85621d883f5adeddcba9dc6f9ea611a0ed7433e85160cfcfc83e0af19'
